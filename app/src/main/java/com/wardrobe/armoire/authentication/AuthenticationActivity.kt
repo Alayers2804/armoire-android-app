f554118152eaaa3c7a9b157fbe356ddbffa4e4e6
alayers2804 <ferdinand69manurung@gmail.com>
2025-05-18 21:33:30 +0700

    update dependencies and layout for fragment

--- app/src/main/java/com/wardrobe/armoire/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/wardrobe/armoire/authentication/AuthenticationActivity.kt
@@ -12,6 +12,15 @@ class AuthenticationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_authentication)
+
+        // Add Fragment Container
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, OnboardingFragment())
+                .commit()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
